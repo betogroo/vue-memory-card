2fda9430e8a0b6d7175af24643f5ae057e0f65f5
betogroo <luizhumberto@gmail.com>
2023-09-15 08:12:19 -0300

    chore: initial commit

---
+++ src/modules/main/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+  <v-container class="d-flex justify-center fill-height">
+    <v-responsive class="text-center">
+      <h1 class="text-center text-h1">Jogo da Memória</h1>
+
+      >
+    </v-responsive>
+  </v-container>
+</template>
